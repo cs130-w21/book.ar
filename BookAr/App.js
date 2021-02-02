6842c8e25c13e3c3374bb12a465bb15b13086cf8
DonLe27 <43871876+DonLe27@users.noreply.github.com>
2021-02-02 11:43:31 -0800

    feat: Add buttons for users to take or upload a picture (#17)
    <!-- Describe the changes you made -->
    Ran create-react-native-app bookar
    Used react native image picker to add buttons to take or upload a picture and get the base65 output
    https://github.com/react-native-image-picker/react-native-image-picker
    
    Fixes
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

---
+++ BookAr/App.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  Platform,
+  SafeAreaView,
+} from 'react-native';
+import {Button} from './src/Button';
+
+import * as ImagePicker from './src/image_picker.ts/';
+
+export default function App() {
+  const [response, setResponse] = React.useState(null);
+
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <Button
+          title="Take image"
+          onPress={() =>
+            ImagePicker.launchCamera(
+              {
+                mediaType: 'photo',
+                includeBase64: true,
+                maxHeight: 200,
+                maxWidth: 200,
+              },
+              (response) => {
+                setResponse(response);
+              },
+            )
+          }
+        />
+
+        <Button
+          title="Select image"
+          onPress={() =>
+            ImagePicker.launchImageLibrary(
+              {
+                mediaType: 'photo',
+                includeBase64: true,
+                maxHeight: 200,
+                maxWidth: 200,
+              },
+              (response) => {
+                setResponse(response);
+              },
+            )
+          }
+        />
+
+
+        <View style={styles.response}>
+          <Text>Res: {JSON.stringify(response)}</Text>
+        </View>
+
+        {response && (
+          <View style={styles.image}>
+            <Image
+              style={{width: 200, height: 200}}
+              source={{uri: response.uri}}
+            />
+          </View>
+        )}
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  button: {
+    marginVertical: 24,
+    marginHorizontal: 24,
+  },
+  image: {
+    marginVertical: 24,
+    alignItems: 'center',
+  },
+  response: {
+    marginVertical: 16,
+    marginHorizontal: 8,
+  },
+});
