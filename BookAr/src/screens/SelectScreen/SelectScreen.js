79c4d97e56f95e523a0a7e29054b776ddc435af4
Bryan Pan <bryanpan342@gmail.com>
2021-02-16 21:31:49 -0800

    feat: implement tab navigator for home, select, and profile screen (#25)
    <!-- Describe the changes you made -->
    
    * implement tab navigator
    * sign out functionality
    
    Fixes #24 
    
    ----
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

---
+++ BookAr/src/screens/SelectScreen/SelectScreen.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import {Text, View, Image, ScrollView, SafeAreaView} from 'react-native';
+import styles from './styles';
+import {Button} from '../../utils/Button';
+import * as ImagePicker from '../../utils/image_picker.ts';
+
+export default function SelectScreen({navigation, extraData}) {
+  const [response, setResponse] = React.useState(null);
+
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <Button
+          title="Take image"
+          onPress={() =>
+            ImagePicker.launchCamera(
+              {
+                mediaType: 'photo',
+                includeBase64: true,
+                maxHeight: 200,
+                maxWidth: 200,
+              },
+              (res) => {
+                setResponse(res);
+              },
+            )
+          }
+        />
+
+        <Button
+          title="Select image"
+          onPress={() =>
+            ImagePicker.launchImageLibrary(
+              {
+                mediaType: 'photo',
+                includeBase64: true,
+                maxHeight: 200,
+                maxWidth: 200,
+              },
+              (res) => {
+                setResponse(res);
+              },
+            )
+          }
+        />
+
+        <View style={styles.response}>
+          <Text>Res: {JSON.stringify(response)}</Text>
+        </View>
+
+        {response && (
+          <View style={styles.image}>
+            <Image
+              style={{width: 200, height: 200}}
+              source={{uri: response.uri}}
+            />
+          </View>
+        )}
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
