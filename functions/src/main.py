dc0e004194ba59035305fbb7e949b3644ef14d56
Bryan Pan <bryanpan342@gmail.com>
2021-02-24 17:38:10 -0800

    bug fix

--- functions/src/main.py
+++ functions/src/main.py
@@ -5,7 +5,7 @@ def make_recommendation(request):
   if request_json and 'books' in request_json:
     books = request_json['books']
     model = BookRecommendation()
-    model.load_model('models/', 'book_rec_model')
+    model.load_model('saved_models/', 'book_rec_model')
     return model.make_recommendation(books)
   else:
     return f'Invalid request body {request_json}'
