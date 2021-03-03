33d3cb5cd0c5c81ae8f363d4fbd630b1ba70ce96
ShichangZh <shichangzh@gmail.com>
2021-03-03 13:07:47 -0800

    feat: add a new recommendation algorithm based on TF-IDF score (#30)
    <!-- Describe the changes you made -->
    
    Fixes #8 
    Fixes #11 
    
    To run the trained model, we follow these three steps (detailed version in Jupyter notebook test2.ipynb)
    
    1. create a model instance and load the trained state
    model = TFIDFBookRec()
    model.load_model(input_model_path, input_model_name)
    
    2. set the user preference with a list of Book instances picked by the user
    model.set_user_preference(user_preferred_books)
    
    3. pass in a list of Book instances and the model will return a recommended one
    book = model.make_recommendation(test_books)
    
    ----
    
    
    
    *By submitting this pull request, I confirm that my contribution is made under the terms of the Apache-2.0 license*

--- RecAlgo/train_and_save_model.py
+++ RecAlgo/train_and_save_model.py
@@ -1,21 +1,29 @@
 import pandas as pd
 import numpy as np
-from models import BookRecommendation
-from utils import *
+from models import PopularityBookRec, TFIDFBookRec
+from utils import data_preprocessing
 
 books = pd.read_csv('./dataset/BX-Books.csv', sep=';', error_bad_lines=False, warn_bad_lines=False, low_memory=False, encoding="latin-1")
 ratings = pd.read_csv('./dataset/BX-Book-Ratings.csv', sep=';', error_bad_lines=False, warn_bad_lines=False, low_memory=False, encoding="latin-1")
 
 books, ratings = data_preprocessing(books, ratings)
 
-ratings_avg = pd.DataFrame(ratings.groupby(['ISBN'])['bookRating'].mean()).rename(columns={'bookRating':'avgRating'})
-ratings_count = pd.DataFrame(ratings.groupby(['ISBN'])['bookRating'].count()).rename(columns={'bookRating':'numRatings'})
-book_scores = ratings_count.merge(ratings_avg, left_index = True, right_on = 'ISBN')
-book_scores = book_scores.merge(books, left_index = True, right_on = 'ISBN')
-
-model = BookRecommendation(book_scores)
+model = TFIDFBookRec()
+model.train_tfidf(books)
+model.train_ratings(books, ratings)
 
 output_path = './saved_models/'
-output_model_name = 'book_rec_model'
+output_model_name = 'tfidf_model'
 model.save_model(output_path, output_model_name)
+
+
+
+
+# Popularity-based model
+model = PopularityBookRec()
+model.train_ratings(books, ratings)
+output_path = './saved_models/'
+output_model_name = 'popularity_model'
+model.save_model(output_path, output_model_name)
+
 
