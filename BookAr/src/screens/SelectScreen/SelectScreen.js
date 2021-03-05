a3093b4e4ab08edadf3ee9a1c3db324881862a52
Arjun Raghavan <arjun.raghavan.2k@gmail.com>
2021-03-04 21:06:37 -0500

    Made things prettier

--- BookAr/src/screens/SelectScreen/SelectScreen.js
+++ BookAr/src/screens/SelectScreen/SelectScreen.js
@@ -1,25 +1,69 @@
 import React, {useEffect, useState, useRef} from 'react';
-import {Text, View, TouchableOpacity, SafeAreaView} from 'react-native';
+import {FlatList, Text, View, TouchableOpacity, SafeAreaView} from 'react-native';
 import {RNCamera} from 'react-native-camera';
 import styles from './styles';
 import * as ImagePicker from '../../utils/image_picker.ts';
 import * as CloudVision from '../../utils/cloud_vision.ts';
+import BookListItem from './BookListItem.js';
 
 export default function SelectScreen({ navigation, extraData }) {
   const [ response, setResponse ] = useState(null);
   const [ focus, setFocus ] = useState(null);
   const [ snapBtnText, setSnapBtnText ] = useState('SNAP');
+  const [ recBooks, setRecBooks ] = useState([
+    {
+      title: "asdf",
+      author: "asdf",
+      coverUrl: "http://books.google.com/books/content?id=grzEQgAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+    },
+    {
+      title: "asdf",
+      author: "asdf",
+      coverUrl: "http://books.google.com/books/content?id=grzEQgAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+    },
+    {
+      title: "asdf",
+      author: "asdf",
+      coverUrl: "http://books.google.com/books/content?id=grzEQgAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+    },
+    {
+      title: "asdf",
+      author: "asdf",
+      coverUrl: "http://books.google.com/books/content?id=grzEQgAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+    },
+    {
+      title: "asdf",
+      author: "asdf",
+      coverUrl: "http://books.google.com/books/content?id=grzEQgAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+    },
+    {
+      title: "asdf",
+      author: "asdf",
+      coverUrl: "http://books.google.com/books/content?id=grzEQgAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+    },
+    {
+      title: "asdf",
+      author: "asdf",
+      coverUrl: "http://books.google.com/books/content?id=grzEQgAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+    },
+    {
+      title: "asdf",
+      author: "asdf",
+      coverUrl: "http://books.google.com/books/content?id=grzEQgAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api"
+    },
+  ]);
 
   const cameraRef = useRef(null);
   const pictureTakenRef = useRef(false);
 
   const takePicture = async () => {
+    console.log('picture', pictureTakenRef.current);
     if (cameraRef.current && pictureTakenRef.current == false) {
       const options = {base64: true, pauseAfterCapture: true}; //PAUSE ALLOWS FOR STATIC IMAGE ON SCREEN
       const data = await cameraRef.current.takePictureAsync(options);
       setSnapBtnText('AGAIN');
       setResponse('Loading...');
-      CloudVision.getRecommendedBooks(data.base64, setResponse);
+      CloudVision.getRecommendedBooks(data.base64, setRecBooks);
     } else {
       cameraRef.current.resumePreview(); //CALL THIS TO RESUME PREVIEW / BE ABLE TO TAKE ANOTHER
       setSnapBtnText('SNAP');
@@ -63,7 +107,7 @@ export default function SelectScreen({ navigation, extraData }) {
                 quality: 1,
               },
               async (data) => {
-                const words = await CloudVision.getRecommendedBooks(data.base64, setResponse);
+                const words = await CloudVision.getRecommendedBooks(data.base64, setRecBooks);
               },
             )
           }
@@ -71,9 +115,12 @@ export default function SelectScreen({ navigation, extraData }) {
           <Text> CHOOSE </Text>
         </TouchableOpacity>
       </View>
-      <View style={styles.response}>
-        <Text>Response: {JSON.stringify(response, null, 2)}</Text>
-      </View>
+      <FlatList
+        style={styles.response}
+        data={recBooks}
+        renderItem={({item}) => (<BookListItem key={item.title} book={item} />)}
+      />
+
     </SafeAreaView>
   );
 }
