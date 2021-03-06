73635dd8e0e4f87a99b1452a4da28c8c782c522c
Bryan Pan <bryanpan342@gmail.com>
2021-03-05 17:27:43 -0800

    reformat input books

--- server/tests/test_app.py
+++ server/tests/test_app.py
@@ -1,41 +1,47 @@
 import json
 
-test_book1 = ['059035342X',
-             "Harry Potter and the Sorcerer's Stone (Harry Potter (Paperback))",
-             'J. K. Rowling',
-             1999,
-             'Arthur A. Levine Books',
-             'http://images.amazon.com/images/P/059035342X.01.LZZZZZZZ.jpg']
-test_book2 = ['0385504209',
-             'The Da Vinci Code',
-             'Dan Brown',
-             None,
-             'Doubleday']
-test_book3 = [None,
-             'To Kill a Mockingbird',
-             'Harper Lee']
+test_book1 = {
+  'isbn': '059035342X',
+  'title': "Harry Potter and the Sorcerer's Stone (Harry Potter (Paperback))",
+  'author': 'J. K. Rowling',
+  'year': 1999,
+  'publisher': 'Arthur A. Levine Books'
+}
+test_book2 = {
+  'isbn': '0385504209',
+  'title': 'The Da Vinci Code',
+  'author': 'Dan Brown',
+}
+test_book3 = {
+  'title': 'To Kill a Mockingbird',
+  'author': 'Harper Lee'
+}
 
 test_books = [test_book1, test_book2, test_book3]
 
-user_preferred_book1 = ['0449005615',
-                        'Seabiscuit: An American Legend',
-                        'LAURA HILLENBRAND',
-                        2002,
-                        'Ballantine Books',
-                        'http://images.amazon.com/images/P/0449005615.01.LZZZZZZZ.jpg']
+user_preferred_book1 = {
+  'isbn': '0449005615',
+  'title': 'Seabiscuit: An American Legend',
+  'author': 'LAURA HILLENBRAND',
+  'year': 2002,
+  'publisher': 'Ballantine Books',
+}
 
-user_preferred_book2 = ['0375726403',
-                         'Empire Falls',
-                         'Richard Russo',
-                         2002,
-                         'Vintage Books USA',
-                         'http://images.amazon.com/images/P/0375726403.01.LZZZZZZZ.jpg']
-user_preferred_book3 = ['0786868716',
-                         'The Five People You Meet in Heaven',
-                         'Mitch Albom',
-                         2003,
-                         'Hyperion',
-                         'http://images.amazon.com/images/P/0786868716.01.LZZZZZZZ.jpg']
+user_preferred_book2 = {
+  'isbn': '0375726403',
+  'title': 'Empire Falls',
+  'author': 'Richard Russo',
+  'year': 2002,
+  'publisher': 'Vintage Books USA',
+}
+
+user_preferred_book3 = {
+  'isbn': '0786868716',
+  'title': 'The Five People You Meet in Heaven',
+  'author': 'Mitch Albom',
+  'year': 2003,
+  'publisher': 'Hyperion',
+}
 
 user_preferred_books = [user_preferred_book1, user_preferred_book2, user_preferred_book3]
 
